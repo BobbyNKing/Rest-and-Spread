9c34f4a773a8f323e9b4e139c4b7deb185c88f15
Bobby aka Kole aka Zodi <32856124+BobbyNKing@users.noreply.github.com>
2024-07-09 23:09:31 -0500

    Add files via upload

---
+++ problems/01-rest-sum.js
@@ -0,0 +1,27 @@
+/*
+Write a function named `restSum` that accepts all incoming parameters and sums
+them.
+
+**Hint**: Use rest parameter syntax!
+*/
+
+// Your code here 
+
+let restSum = (...allParam) => {
+  let sum = 0;
+  allParam.forEach(function(num) {
+    sum += num;
+  })
+  return sum;
+}
+
+console.log(restSum(3,5,6));                     // => 14
+// console.log(restSum(1, 2, 3, 4, 5, 6, 7, 8, 9)); // => 45
+// console.log(restSum(0));                         // => 0
+
+
+/******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
+
+try {
+  module.exports = restSum;
+} catch {}
